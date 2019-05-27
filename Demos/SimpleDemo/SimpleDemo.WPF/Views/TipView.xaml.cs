8d0e4a09e370edca6a62d98625e2e1ea6206c6cd
Mark Kromis <mkromis@outlook.com>
2019-05-26 20:18:30 -0400

    Check view model for type instead of attribs on views

--- Demos/SimpleDemo/SimpleDemo.WPF/Views/TipView.xaml.cs
+++ Demos/SimpleDemo/SimpleDemo.WPF/Views/TipView.xaml.cs
@@ -1,32 +1,12 @@
-using MinoriEditorStudio.Framework.Attributes;
-using MvvmCross.Platforms.Wpf.Presenters.Attributes;
 using MvvmCross.Platforms.Wpf.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SimpleDemo.WPF.Views
 {
     /// <summary>
     /// Interaction logic for TipView.xaml
     /// </summary>
-    [DocumentView]
     public partial class TipView : MvxWpfView
     {
-        public TipView()
-        {
-            InitializeComponent();
-        }
+        public TipView() => InitializeComponent();
     }
 }
