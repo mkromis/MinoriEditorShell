10f44444cfe1b7029562856e022d1585658ce2c8
Mark Kromis <mkromis@outlook.com>
2019-05-27 15:28:25 -0400

    Add simple ribbon demo

---
+++ SimpleDemo.RibbonWPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+using Fluent;
+using MvvmCross.Platforms.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleDemo.RibbonWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MvxWindow
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
---
+++ SimpleDemo.RibbonWPF/Views/TipView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace SimpleDemo.RibbonWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for TipView.xaml
+    /// </summary>
+    public partial class TipView : MvxWpfView
+    {
+        public TipView() => InitializeComponent();
+    }
+}
