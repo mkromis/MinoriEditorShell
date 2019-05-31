893328e6d3c3a9cd6857ce64d6f467dbbbc48d94
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-05-30 22:01:31 -0400

    add initial VirtualCanvas from sample

---
+++ Modules/MinoriEditorStudio.VirtualCanvas/ViewModels/VirtualCanvasViewModel.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Window1.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using MinoriEditorStudio.VirtualCanvas.Gestures;
+using MvvmCross.ViewModels;
+using System.Windows.Controls;
+
+namespace MinoriEditorStudio.VirtualCanvas.ViewModels
+{
+    /// <summary>
+    /// This demo shows the VirtualCanvas managing up to 50,000 random WPF shapes providing smooth scrolling and
+    /// zooming while creating those shapes on the fly.  This helps make a WPF canvas that is a lot more
+    /// scalable.
+    /// </summary>
+    public class VirtualCanvasViewModel : MvxViewModel
+
+    {
+        public MapZoom Zoom { get; protected set; }
+        public Pan Pan { get; protected set; }
+        public RectangleSelectionGesture RectZoom { get; protected set; }
+        public AutoScroll AutoScroll { get; protected set; }
+
+        public Controls.VirtualCanvas Graph { get; protected set; }
+
+        public VirtualCanvasViewModel()
+        {
+            Views.VirtualCanvas canvas = new Views.VirtualCanvas();
+            //Graph = canvas.Graph;
+            //View = canvas;
+
+            Canvas target = Graph.ContentCanvas;
+            Zoom = new MapZoom(target);
+            Pan = new Pan(target, Zoom);
+            AutoScroll = new AutoScroll(target, Zoom);
+            RectZoom = new RectangleSelectionGesture(target, Zoom);
+        }
+    }
+}
