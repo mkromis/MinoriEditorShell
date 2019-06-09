d12f1059202cc7b33930cbca687aa54de1f53759
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-06-09 16:40:11 -0400

    First attempt at setting notification via messenger

--- Modules/MinoriEditorStudio/Messages/ThemeChangeMessage.cs
+++ Modules/MinoriEditorStudio/Messages/ThemeChangeMessage.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MinoriEditorStudio.Modules.Themes.Services
+namespace MinoriEditorStudio.Messages
 {
     public class ThemeChangeMessage : MvxMessage
     {
