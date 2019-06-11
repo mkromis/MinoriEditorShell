3a2befe829db0384bf8fdac4cb3b97d7828702c6
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-06-10 22:08:22 -0400

    change to rectf / interface

--- Modules/MinoriEditorStudio.VirtualCanvas/Platforms/Wpf/Models/Quadrant.cs
+++ Modules/MinoriEditorStudio.VirtualCanvas/Platforms/Wpf/Models/Quadrant.cs
@@ -3,13 +3,14 @@
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
+using MinoriEditorStudio.VirtualCanvas.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace MinoriEditorStudio.VirtualCanvas.Models
 {
@@ -19,45 +20,49 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
     /// and each quadrant is split up into four child Quadrants recurrsively.  Objects that overlap more than
     /// one quadrant are stored in the _nodes list for this Quadrant.
     /// </summary>
-    public class Quadrant<T>
+    public class Quadrant<T> : IQuadrant<T>
     {
         /// <summary>
         /// The bounds of this quadrant
         /// </summary>
-        public Rect Bounds { get; private set; } // quadrant bounds.
+        public RectangleF Bounds { get; private set; } // quadrant bounds.
 
-        public QuadNode<T> Nodes { get; private set; } // nodes that overlap the sub quadrant boundaries.
+        public IQuadNode<T> Nodes { get; private set; } // nodes that overlap the sub quadrant boundaries.
 
         // The quadrant is subdivided when nodes are inserted that are 
         // completely contained within those subdivisions.
-        public Quadrant<T> TopLeft { get; private set; }
-        public Quadrant<T> TopRight { get; private set; }
-        public Quadrant<T> BottomLeft { get; private set; }
-        public Quadrant<T> BottomRight { get; private set; }
+        public IQuadrant<T> TopLeft { get; private set; }
+        public IQuadrant<T> TopRight { get; private set; }
+        public IQuadrant<T> BottomLeft { get; private set; }
+        public IQuadrant<T> BottomRight { get; private set; }
 
         /// <summary>
         /// Statictial information for rendering use.
         /// </summary>
         /// <param name="c"></param>
-        public void ShowQuadTree(Canvas c)
+        public void ShowQuadTree(Object o)
         {
-            Rectangle r = new Rectangle
+            if (o is Canvas c)
             {
-                Width = Bounds.Width,
-                Height = Bounds.Height
-            };
+                //Rectangle r = new Rectangle
+                //{
+                //    Width = Bounds.Width,
+                //    Height = Bounds.Height
+                //};
 
-            Canvas.SetLeft(r, Bounds.Left);
-            Canvas.SetTop(r, Bounds.Top);
-            r.Stroke = Brushes.DarkRed;
-            r.StrokeThickness = 1;
-            r.StrokeDashArray = new DoubleCollection(new Double[] { 2.0, 3.0 });
-            c.Children.Add(r);
+                //Canvas.SetLeft(r, Bounds.);
+                //Canvas.SetTop(r, Bounds.Top);
+                //r.Stroke = Brushes.DarkRed;
+                //r.StrokeThickness = 1;
+                //r.StrokeDashArray = new DoubleCollection(new Double[] { 2.0, 3.0 });
+                //c.Children.Add(r);
 
-            TopLeft?.ShowQuadTree(c);
-            TopRight?.ShowQuadTree(c);
-            BottomLeft?.ShowQuadTree(c);
-            BottomRight?.ShowQuadTree(c);
+                //TopLeft?.ShowQuadTree(c);
+                //TopRight?.ShowQuadTree(c);
+                //BottomLeft?.ShowQuadTree(c);
+                //BottomRight?.ShowQuadTree(c);
+            }
+            throw new NotImplementedException();
         }
 
         /// <summary>
@@ -66,7 +71,7 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
         /// </summary>
         /// <param name="parent">The parent quadrant (if any)</param>
         /// <param name="bounds">The bounds of this quadrant</param>
-        public Quadrant(Quadrant<T> parent, Rect bounds)
+        public Quadrant(IQuadrant<T> parent, RectangleF bounds)
         {
             Parent = parent;
             Debug.Assert(bounds.Width != 0 && bounds.Height != 0);
@@ -81,7 +86,7 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
         /// <summary>
         /// The parent Quadrant or null if this is the root
         /// </summary>
-        internal Quadrant<T> Parent { get; }
+        internal IQuadrant<T> Parent { get; }
 
         /// <summary>
         /// Insert the given node
@@ -89,7 +94,7 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
         /// <param name="node">The node </param>
         /// <param name="bounds">The bounds of that node</param>
         /// <returns></returns>
-        internal Quadrant<T> Insert(T node, Rect bounds)
+        public IQuadrant<T> Insert(T node, RectangleF bounds)
         {
             Debug.Assert(bounds.Width != 0 && bounds.Height != 0);
             if (bounds.Width == 0 || bounds.Height == 0)
@@ -98,12 +103,12 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
                 throw new ArgumentException("Bounds of quadrant cannot be zero width or height");
             }
 
-            Double w = Bounds.Width / 2;
+            Single w = Bounds.Width / 2;
             if (w == 0)
             {
                 w = 1;
             }
-            Double h = Bounds.Height / 2;
+            Single h = Bounds.Height / 2;
             if (h == 0)
             {
                 h = 1;
@@ -112,12 +117,12 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
             // assumption that the Rect struct is almost as fast as doing the operations
             // manually since Rect is a value type.
 
-            Rect topLeft = new Rect(Bounds.Left, Bounds.Top, w, h);
-            Rect topRight = new Rect(Bounds.Left + w, Bounds.Top, w, h);
-            Rect bottomLeft = new Rect(Bounds.Left, Bounds.Top + h, w, h);
-            Rect bottomRight = new Rect(Bounds.Left + w, Bounds.Top + h, w, h);
+            RectangleF topLeft = new RectangleF(Bounds.Left, Bounds.Top, w, h);
+            RectangleF topRight = new RectangleF(Bounds.Left + w, Bounds.Top, w, h);
+            RectangleF bottomLeft = new RectangleF(Bounds.Left, Bounds.Top + h, w, h);
+            RectangleF bottomRight = new RectangleF(Bounds.Left + w, Bounds.Top + h, w, h);
 
-            Quadrant<T> child = null;
+            IQuadrant<T> child = null;
 
             // See if any child quadrants completely contain this node.
             if (topLeft.Contains(bounds))
@@ -167,7 +172,7 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
                 else
                 {
                     // link up in circular link list.
-                    QuadNode<T> x = Nodes;
+                    IQuadNode<T> x = Nodes;
                     n.Next = x.Next;
                     x.Next = n;
                 }
@@ -182,19 +187,19 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
         /// </summary>
         /// <param name="nodes">List of nodes found in the given bounds</param>
         /// <param name="bounds">The bounds that contains the nodes you want returned</param>
-        internal void GetIntersectingNodes(List<QuadNode<T>> nodes, Rect bounds)
+        public void GetIntersectingNodes(IList<IQuadNode<T>> nodes, RectangleF bounds)
         {
             if (bounds.IsEmpty) { return; }
-            Double w = Bounds.Width / 2;
-            Double h = Bounds.Height / 2;
+            Single w = Bounds.Width / 2;
+            Single h = Bounds.Height / 2;
 
             // assumption that the Rect struct is almost as fast as doing the operations
             // manually since Rect is a value type.
 
-            Rect topLeft = new Rect(Bounds.Left, Bounds.Top, w, h);
-            Rect topRight = new Rect(Bounds.Left + w, Bounds.Top, w, h);
-            Rect bottomLeft = new Rect(Bounds.Left, Bounds.Top + h, w, h);
-            Rect bottomRight = new Rect(Bounds.Left + w, Bounds.Top + h, w, h);
+            RectangleF topLeft = new RectangleF(Bounds.Left, Bounds.Top, w, h);
+            RectangleF topRight = new RectangleF(Bounds.Left + w, Bounds.Top, w, h);
+            RectangleF bottomLeft = new RectangleF(Bounds.Left, Bounds.Top + h, w, h);
+            RectangleF bottomRight = new RectangleF(Bounds.Left + w, Bounds.Top + h, w, h);
 
             // See if any child quadrants completely contain this node.
             if (topLeft.IntersectsWith(bounds) && TopLeft != null)
@@ -227,11 +232,11 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
         /// <param name="last">The last QuadNode in a circularly linked list</param>
         /// <param name="nodes">The resulting nodes are added to this list</param>
         /// <param name="bounds">The bounds to test against each node</param>
-        static void GetIntersectingNodes(QuadNode<T> last, List<QuadNode<T>> nodes, Rect bounds)
+        public void GetIntersectingNodes(IQuadNode<T> last, IList<IQuadNode<T>> nodes, RectangleF bounds)
         {
             if (last != null)
             {
-                QuadNode<T> n = last;
+                IQuadNode<T> n = last;
                 do
                 {
                     n = n.Next; // first node.
@@ -248,19 +253,19 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
         /// </summary>
         /// <param name="bounds">The bounds to test</param>
         /// <returns>boolean</returns>
-        internal Boolean HasIntersectingNodes(Rect bounds)
+        public Boolean HasIntersectingNodes(RectangleF bounds)
         {
             if (bounds.IsEmpty) { return false; }
-            Double w = Bounds.Width / 2;
-            Double h = Bounds.Height / 2;
+            Single w = Bounds.Width / 2;
+            Single h = Bounds.Height / 2;
 
             // assumption that the Rect struct is almost as fast as doing the operations
             // manually since Rect is a value type.
 
-            Rect topLeft = new Rect(Bounds.Left, Bounds.Top, w, h);
-            Rect topRight = new Rect(Bounds.Left + w, Bounds.Top, w, h);
-            Rect bottomLeft = new Rect(Bounds.Left, Bounds.Top + h, w, h);
-            Rect bottomRight = new Rect(Bounds.Left + w, Bounds.Top + h, w, h);
+            RectangleF topLeft = new RectangleF(Bounds.Left, Bounds.Top, w, h);
+            RectangleF topRight = new RectangleF(Bounds.Left + w, Bounds.Top, w, h);
+            RectangleF bottomLeft = new RectangleF(Bounds.Left, Bounds.Top + h, w, h);
+            RectangleF bottomRight = new RectangleF(Bounds.Left + w, Bounds.Top + h, w, h);
 
             Boolean found = false;
 
@@ -297,11 +302,11 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
         /// <param name="last">The last node in the circularly linked list.</param>
         /// <param name="bounds">Bounds to test</param>
         /// <returns>Return true if a node in the list intersects the bounds</returns>
-        static Boolean HasIntersectingNodes(QuadNode<T> last, Rect bounds)
+        public Boolean HasIntersectingNodes(IQuadNode<T> last, RectangleF bounds)
         {
             if (last != null)
             {
-                QuadNode<T> n = last;
+                IQuadNode<T> n = last;
                 do
                 {
                     n = n.Next; // first node.
@@ -319,12 +324,12 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
         /// </summary>
         /// <param name="node">The node to remove</param>
         /// <returns>Returns true if the node was found and removed.</returns>
-        internal Boolean RemoveNode(T node)
+        public Boolean RemoveNode(T node)
         {
             Boolean rc = false;
             if (Nodes != null)
             {
-                QuadNode<T> p = Nodes;
+                IQuadNode<T> p = Nodes;
                 while (!p.Next.Node.Equals(node) && p.Next != Nodes)
                 {
                     p = p.Next;
@@ -332,7 +337,7 @@ namespace MinoriEditorStudio.VirtualCanvas.Models
                 if (p.Next.Node.Equals(node))
                 {
                     rc = true;
-                    QuadNode<T> n = p.Next;
+                    IQuadNode<T> n = p.Next;
                     if (p == n)
                     {
                         // list goes to empty
---
+++ Modules/MinoriEditorStudio.VirtualCanvas/Service/IQuadNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using System.Windows;
+
+namespace MinoriEditorStudio.VirtualCanvas.Services
+{
+    public interface IQuadNode<T>
+    {
+        RectangleF Bounds { get; }
+        IQuadNode<T> Next { get; set; }
+        T Node { get; set; }
+    }
+}
