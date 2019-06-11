f7a0df990bcdf102f3cdb6aeeace62887c869372
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-06-10 22:10:56 -0400

    Move view models to core

--- Demos/MinoriDemo/MinoriDemo.Core/ViewModels/ToolSampleViewModel.cs
+++ Demos/MinoriDemo/MinoriDemo.Core/ViewModels/ToolSampleViewModel.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MinoriDemo.RibbonWPF.Modules.VirtualCanvas.Models
+namespace MinoriDemo.Core.Models
 {
     class ToolSampleViewModel : Tool
     {
