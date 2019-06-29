837f8ad7dc86f8c8ec8a80250537a473be920813
Mark Kromis <mkromis@outlook.com>
2019-06-29 15:23:49 -0400

    Add test to verify ITheme.HasRibbon

---
+++ Tests/MinoriEditorStudio.CoreTests.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MinoriEditorStudio.CoreTests.Wpf")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinoriEditorStudio.CoreTests.Wpf")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("057b4fdd-4e41-47bf-8da6-6955523d4dd1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
