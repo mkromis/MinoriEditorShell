73e5e3a64aced325cbf0a921841a4f68f40649e2
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-06-29 15:32:09 -0400

    Add test to verify ribbon is there

---
+++ Tests/MinoriEditorStudio.RibbonTests.Wpf/Platforms/Wpf/Extensions/IThemeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinoriEditorStudio.Platforms.Wpf.Extensions;
+using MinoriEditorStudio.Platforms.Wpf.Themes;
+using MinoriEditorStudio.Services;
+
+namespace MinoriEditorStudio.RibbonTests.Wpf.Platforms.Wpf.Extensions
+{
+    [TestClass]
+    public class IThemeTests    {
+        [TestMethod]
+        public void NoRibbonTest()
+        {
+            BlueTheme blue = new BlueTheme();
+            Assert.IsTrue(blue.HasRibbon());
+        }
+    }
+}
