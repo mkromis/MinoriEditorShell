b94da1677e4904b18e0e2c96136c27601aecc429
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-07-13 15:29:30 -0400

    try to get all interfaces.

---
+++ Modules/MinoriEditorStudio/Platforms/Wpf/Extensions/IocProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross;
+using MvvmCross.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MinoriEditorStudio.Platforms.Wpf.Extensions
+{
+    public static class IocProviderExtensions
+    {
+        public static IEnumerable<T> GetAll<T>(this IMvxIoCProvider provider) where T : class
+        {
+            IEnumerable<MvxTypeExtensions.ServiceTypeAndImplementationTypePair> result = Application.Current.GetType().Assembly.CreatableTypes().AsInterfaces(typeof(T));
+            return result.Select(x => x.GetType() as T).Where(x => x != null);
+        }
+    }
+}
