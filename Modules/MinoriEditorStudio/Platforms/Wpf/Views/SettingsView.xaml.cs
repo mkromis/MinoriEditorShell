7d066e5abc34f30bf56807009dca9282105d0fa8
Mark Kromis <mkromis@outlook.com>
2019-07-13 15:27:25 -0400

    adjust window to use MesWindow

--- Modules/MinoriEditorStudio/Platforms/Wpf/Views/SettingsView.xaml.cs
+++ Modules/MinoriEditorStudio/Platforms/Wpf/Views/SettingsView.xaml.cs
@@ -5,7 +5,7 @@ namespace MinoriEditorStudio.Platforms.Wpf.Views
     /// <summary>
     /// Interaction logic for SettingsView.xaml
     /// </summary>
-    public partial class SettingsView : Window
+    public partial class SettingsView
     {
         public SettingsView()
         {
