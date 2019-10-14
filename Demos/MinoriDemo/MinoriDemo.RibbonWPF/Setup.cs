6df28de6cedd7b7e9371f99ac965bf60f916046c
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-10-13 22:46:31 -0400

    add to demo to show how to add to themes.

---
+++ Demos/MinoriDemo/MinoriDemo.RibbonWPF/Setup.cs
@@ -0,0 +1,35 @@
+﻿using MinoriEditorShell.Platforms.Wpf;
+using MinoriEditorShell.Platforms.Wpf.Themes;
+using MinoriEditorShell.Services;
+using MvvmCross;
+using System;
+
+namespace MinoriDemo.RibbonWPF
+{
+    internal class Setup : MesWpfSetup<Core.App>
+    {
+        protected override void InitializeLastChance()
+        {
+            base.InitializeLastChance();
+
+            IThemeManager manager = Mvx.IoCProvider.Resolve<IThemeManager>();
+            foreach (ITheme theme in manager.Themes)
+            {
+                switch (theme)
+                {
+                    case BlueTheme blue:
+                        blue.Add(new Uri("pack://application:,,,/ColorPickerLib;component/Themes/LightBrushs.xaml"));
+                        break;
+                    case LightTheme light:
+                        light.Add(new Uri("pack://application:,,,/ColorPickerLib;component/Themes/LightBrushs.xaml"));
+                        break;
+                    case DarkTheme dark:
+                        dark.Add(new Uri("pack://application:,,,/ColorPickerLib;component/Themes/DarkBrushs.xaml"));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
