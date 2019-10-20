e6a8099f91110d3131ef7c0670ef6e030c5f777b
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-10-19 23:05:40 -0400

    adjust documentation to reflect you don't need to use MesDocuemnt

--- Demos/MinoriDemo/MinoriDemo.RibbonWPF/Views/VirtualCanvasView.xaml.cs
+++ Demos/MinoriDemo/MinoriDemo.RibbonWPF/Views/VirtualCanvasView.xaml.cs
@@ -1,20 +1,5 @@
-﻿using MinoriEditorShell.VirtualCanvas.Platforms.Wpf.Gestures;
+﻿
 using MinoriEditorShell.VirtualCanvas.Services;
-using MinoriEditorShell.VirtualCanvas.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MinoriDemo.RibbonWPF.Views
 {
@@ -29,7 +14,7 @@ namespace MinoriDemo.RibbonWPF.Views
 
             DataContextChanged += (s, e) =>
             {
-                MesVirtualCanvasViewModel dc = (MesVirtualCanvasViewModel)DataContext;
+                IMesVirtualCanvas dc = (IMesVirtualCanvas)DataContext;
                 Graph.UseDefaultControls(dc);
             };
         }
