b6c50493820ca15a8b53d4c0c8db124668b2f718
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-10-27 23:25:38 -0400

    Add ability to set background canvas color

--- Modules/MinoriEditorShell.VirtualCanvas/Platforms/Wpf/Controls/MesContentCanvas.cs
+++ Modules/MinoriEditorShell.VirtualCanvas/Platforms/Wpf/Controls/MesContentCanvas.cs
@@ -1,6 +1,7 @@
 ﻿using MinoriEditorShell.VirtualCanvas.Services;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 using System.Windows.Controls;
 
@@ -11,5 +12,16 @@ namespace MinoriEditorShell.VirtualCanvas.Platforms.Wpf.Controls
     /// </summary>
     public class MesContentCanvas : Canvas, IMesContentCanvas
     {
+        /// <summary>
+        /// Sets the background color of the canvas object
+        /// </summary>
+        /// <param name="color"></param>
+        public void SetCanvasBackgroundColor(Color color)
+        {
+            System.Windows.Media.Color newcolor = 
+                System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+            System.Windows.Media.Brush brush = new System.Windows.Media.SolidColorBrush(newcolor);
+            Background = brush;
+        }
     }
 }
--- Modules/MinoriEditorShell.VirtualCanvas/Services/IMesVirtualCanvasControl.cs
+++ Modules/MinoriEditorShell.VirtualCanvas/Services/IMesVirtualCanvasControl.cs
@@ -4,6 +4,17 @@ using System.Drawing;
 
 namespace MinoriEditorShell.VirtualCanvas.Services
 {
+    public class VisualChangeEventArgs : EventArgs
+    {
+        public Int32 Added { get; set; }
+        public Int32 Removed { get; set; }
+        public VisualChangeEventArgs(Int32 added, Int32 removed)
+        {
+            Added = added;
+            Removed = removed;
+        }
+    }
+
     public interface IMesVirtualCanvasControl
     {
         IMesContentCanvas ContentCanvas { get; }
@@ -14,6 +25,9 @@ namespace MinoriEditorShell.VirtualCanvas.Services
 
         ObservableCollection<IMesVirtualChild> VirtualChildren { get; }
         SizeF Extent { get; }
+        Int32 LiveVisualCount { get; }
+
+        event EventHandler<VisualChangeEventArgs> VisualsChanged;
 
         void AddVirtualChild(IMesVirtualChild shape);
     }
