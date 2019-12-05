1522acd9201f390357032dded823294f05d46ee9
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2019-12-04 22:46:35 -0500

    playing with new color formats

--- Modules/MinoriEditorShell/Platforms/Wpf/Themes/MesThemeBase.cs
+++ Modules/MinoriEditorShell/Platforms/Wpf/Themes/MesThemeBase.cs
@@ -8,7 +8,7 @@ namespace MinoriEditorShell.Platforms.Wpf.Themes
     /// <summary>
     /// Base theme to initilizer
     /// </summary>
-    public class MesThemeBase : IMesTheme
+    public abstract class MesThemeBase : IMesTheme
     {
         readonly List<Uri> _resources;
 
@@ -25,6 +25,9 @@ namespace MinoriEditorShell.Platforms.Wpf.Themes
             _resources = new List<Uri> {
                 new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Controls.xaml"),
                 new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Fonts.xaml"),
+                new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Themes/Light.Blue.xaml"),
+
+                new Uri("pack://application:,,,/MahApps.Metro;component/Styles/VS/Colors.xaml"),
             };
         }
     }
