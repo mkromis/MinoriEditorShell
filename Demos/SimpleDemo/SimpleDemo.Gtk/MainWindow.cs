9509086e7cda9809095a3809114f5726143b3fc6
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2020-04-25 12:41:05 -0400

    playing with glade test

--- Demos/SimpleDemo/SimpleDemo.Gtk/MainWindow.cs
+++ Demos/SimpleDemo/SimpleDemo.Gtk/MainWindow.cs
@@ -1,13 +1,18 @@
-﻿using MinoriEditorShell.Platforms.Gtk.Views;
+﻿using Gtk;
+using MinoriEditorShell.Platforms.Gtk.Views;
 using System;
+using UI = Gtk.Builder.ObjectAttribute;
 
 namespace SimpleDemo.Gtk
 {
     public class MainWindow : MesGtkWindow
     {
-        public MainWindow()
+        public MainWindow() : this(new Builder("SimpleDemo.Gtk.MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) :
+            base(builder.GetObject("MainWindow").Handle)
         {
-            Title = "SimpleDemo";
+            builder.Autoconnect(this);
         }
     }
 }
--- Modules/MinoriEditorShell.Platforms.Gtk/Views/MesGtkWindow.cs
+++ Modules/MinoriEditorShell.Platforms.Gtk/Views/MesGtkWindow.cs
@@ -5,10 +5,9 @@ namespace MinoriEditorShell.Platforms.Gtk.Views
 {
     public class MesGtkWindow : global::Gtk.Window, IMesWindow
     {
-        public MesGtkWindow() : base(global::Gtk.WindowType.Toplevel)
-        {
+        public MesGtkWindow() : base(global::Gtk.WindowType.Toplevel) { }
 
-        }
+        public MesGtkWindow(IntPtr ptr) : base(ptr) { }
 
         public String DisplayName {
             get => Title;
