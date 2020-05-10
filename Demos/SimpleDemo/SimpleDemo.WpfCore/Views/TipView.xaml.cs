c172216dae57f937e3ae8041a33d17391d33fcb0
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2020-05-10 17:13:07 -0400

    fix incorrect TipView

--- Demos/SimpleDemo/SimpleDemo.WpfCore/Views/TipView.xaml.cs
+++ Demos/SimpleDemo/SimpleDemo.WpfCore/Views/TipView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvvmCross.Platforms.Wpf.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -16,7 +17,7 @@ namespace SimpleDemo.WpfCore.Views
     /// <summary>
     /// Interaction logic for TipView.xaml
     /// </summary>
-    public partial class TipView : UserControl
+    public partial class TipView : MvxWpfView
     {
         public TipView()
         {
