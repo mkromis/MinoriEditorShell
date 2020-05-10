34d3ae5b635e3c4e46eafac9cb911d3ab8a85fae
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2020-05-10 19:09:40 -0400

    copy wpf to core

---
+++ Demos/MinoriDemo/MinoriDemo.WpfCore/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace MinoriDemo.WpfCore.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : MvxWpfView
+    {
+        public MainView() => InitializeComponent();
+    }
+}
