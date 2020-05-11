150522c91326b8bada5bd49a69194dc920e9bbce
Mark Kromis <mkromis@outlook.com>
2020-05-10 20:28:18 -0400

    try not to automatic genreate version

--- Demos/SimpleDemo/SimpleDemo.WpfCore/AssemblyInfo.cs
+++ Demos/SimpleDemo/SimpleDemo.WpfCore/AssemblyInfo.cs
@@ -1,6 +1,18 @@
 using System.Reflection;
 using System.Windows;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleDemo.WPF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleDemo.WPF")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
