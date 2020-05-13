df277a4a7caa2e6312c505ee8930a50bf6bc8569
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2020-05-12 21:11:47 -0400

    add documentation

--- Modules/MinoriEditorShell.Platforms.Wpf/Themes/MesLightTheme.cs
+++ Modules/MinoriEditorShell.Platforms.Wpf/Themes/MesLightTheme.cs
@@ -1,14 +1,20 @@
-using MinoriEditorShell.Platforms.Wpf.Extensions;
-using MinoriEditorShell.Services;
 using System;
-using System.Collections.Generic;
 
 namespace MinoriEditorShell.Platforms.Wpf.Themes
 {
+    /// <summary>
+    /// Light theme definition
+    /// </summary>
     public class MesLightTheme : MesThemeBase
     {
+        /// <summary>
+        /// Name of the light theme
+        /// </summary>
         public override String Name => Properties.Resources.ThemeLightName;
 
+        /// <summary>
+        /// Light theme constructor
+        /// </summary>
         public MesLightTheme() : base()
         {
             Add(new Uri("pack://application:,,,/MinoriEditorShell.Platforms.Wpf;component/Themes/LightTheme.xaml"));
