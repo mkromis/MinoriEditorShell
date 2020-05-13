e93c2a0f1863422c513940f6142d63fbbc15331d
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2020-05-12 21:36:56 -0400

    remove interactions for now. not coreapp happy

--- Modules/MinoriEditorShell.Platforms.Wpf/Services/MesInputManager.cs
+++ Modules/MinoriEditorShell.Platforms.Wpf/Services/MesInputManager.cs
@@ -2,55 +2,55 @@ using System;
 using System.ComponentModel.Composition;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Interactivity;
+//using System.Windows.Interactivity;
 
 namespace MinoriEditorShell.Platforms.Wpf.Services
 {
-	[Export(typeof(IMesInputManager))]
-	public class MesInputManager : IMesInputManager
-	{
-		public void SetShortcut(DependencyObject view, InputGesture gesture, object handler)
-		{
-			var inputBindingTrigger = new MesInputBindingTrigger();
-			inputBindingTrigger.InputBinding = new InputBinding(new RoutedCommand(), gesture);
+//	[Export(typeof(IMesInputManager))]
+//	public class MesInputManager : IMesInputManager
+//	{
+//		public void SetShortcut(DependencyObject view, InputGesture gesture, object handler)
+//		{
+//			var inputBindingTrigger = new MesInputBindingTrigger();
+//			inputBindingTrigger.InputBinding = new InputBinding(new RoutedCommand(), gesture);
 
-			//var target = ViewLocator.LocateForModel(handler, null, null);
-			Interaction.GetTriggers(view).Add(inputBindingTrigger);
+//			//var target = ViewLocator.LocateForModel(handler, null, null);
+//			Interaction.GetTriggers(view).Add(inputBindingTrigger);
 
-			inputBindingTrigger.Actions.Add(new TestTriggerAction(handler));
-		}
+//			inputBindingTrigger.Actions.Add(new TestTriggerAction(handler));
+//		}
 
-		public void SetShortcut(InputGesture gesture, object handler)
-		{
-			SetShortcut(Application.Current.MainWindow, gesture, handler);
-		}
+//		public void SetShortcut(InputGesture gesture, object handler)
+//		{
+//			SetShortcut(Application.Current.MainWindow, gesture, handler);
+//		}
 
-		private class TestTriggerAction : TriggerAction<FrameworkElement>
-		{
-			private readonly object _handler;
+//		private class TestTriggerAction : TriggerAction<FrameworkElement>
+//		{
+//			private readonly object _handler;
 
-			public TestTriggerAction(object handler)
-			{
-				_handler = handler;
-			}
+//			public TestTriggerAction(object handler)
+//			{
+//				_handler = handler;
+//			}
 
-			protected override void Invoke(object parameter)
-			{
-                throw new NotImplementedException();
-#warning Invoke
-#if false
-                var context = new ActionExecutionContext
-                {
-                    Target = _handler,
-                    Message = new ActionMessage { MethodName = "Execute" },
-                    Method = _handler.GetType().GetMethod("Execute")
-                };
-			    ActionMessage.PrepareContext(context);
+//			protected override void Invoke(object parameter)
+//			{
+//                throw new NotImplementedException();
+//#warning Invoke
+//#if false
+//                var context = new ActionExecutionContext
+//                {
+//                    Target = _handler,
+//                    Message = new ActionMessage { MethodName = "Execute" },
+//                    Method = _handler.GetType().GetMethod("Execute")
+//                };
+//			    ActionMessage.PrepareContext(context);
 
-                if (context.CanExecute())
-				    ActionMessage.InvokeAction(context);
-#endif
-			}
-		}
-	}
+//                if (context.CanExecute())
+//				    ActionMessage.InvokeAction(context);
+//#endif
+//			}
+//		}
+//	}
 }
