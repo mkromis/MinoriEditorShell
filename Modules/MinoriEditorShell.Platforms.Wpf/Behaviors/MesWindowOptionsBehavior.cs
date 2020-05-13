bdb88ca7b378b176b67aa5c0fdcb1b84781a5c75
Mark Kromis <mkromis@outlook.com>
2020-05-12 21:36:56 -0400

    remove interactions for now. not coreapp happy

--- Modules/MinoriEditorShell.Platforms.Wpf/Behaviors/MesWindowOptionsBehavior.cs
+++ Modules/MinoriEditorShell.Platforms.Wpf/Behaviors/MesWindowOptionsBehavior.cs
@@ -1,105 +1,105 @@
 using MinoriEditorShell.Platforms.Wpf.Win32;
 using System;
 using System.Windows;
-using System.Windows.Interactivity;
+//using System.Windows.Interactivity;
 using System.Windows.Interop;
 
 namespace MinoriEditorShell.Platforms.Wpf.Behaviors
 {
-    public class MesWindowOptionsBehavior : Behavior<Window>
-    {
-        public static readonly DependencyProperty ShowIconProperty = DependencyProperty.Register(
-            "ShowIcon", typeof(Boolean), typeof(MesWindowOptionsBehavior), 
-            new PropertyMetadata(true, OnWindowOptionChanged));
+    //public class MesWindowOptionsBehavior : Behavior<Window>
+    //{
+    //    public static readonly DependencyProperty ShowIconProperty = DependencyProperty.Register(
+    //        "ShowIcon", typeof(Boolean), typeof(MesWindowOptionsBehavior), 
+    //        new PropertyMetadata(true, OnWindowOptionChanged));
 
-        public Boolean ShowIcon
-        {
-            get => (Boolean)GetValue(ShowIconProperty);
-            set => SetValue(ShowIconProperty, value);
-        }
+    //    public Boolean ShowIcon
+    //    {
+    //        get => (Boolean)GetValue(ShowIconProperty);
+    //        set => SetValue(ShowIconProperty, value);
+    //    }
 
-        public static readonly DependencyProperty ShowMinimizeBoxProperty = DependencyProperty.Register(
-            "ShowMinimizeBox", typeof(Boolean), typeof(MesWindowOptionsBehavior),
-            new PropertyMetadata(true, OnWindowOptionChanged));
+    //    public static readonly DependencyProperty ShowMinimizeBoxProperty = DependencyProperty.Register(
+    //        "ShowMinimizeBox", typeof(Boolean), typeof(MesWindowOptionsBehavior),
+    //        new PropertyMetadata(true, OnWindowOptionChanged));
 
-        public Boolean ShowMinimizeBox
-        {
-            get => (Boolean)GetValue(ShowMinimizeBoxProperty);
-            set => SetValue(ShowMinimizeBoxProperty, value);
-        }
+    //    public Boolean ShowMinimizeBox
+    //    {
+    //        get => (Boolean)GetValue(ShowMinimizeBoxProperty);
+    //        set => SetValue(ShowMinimizeBoxProperty, value);
+    //    }
 
-        public static readonly DependencyProperty ShowMaximizeBoxProperty = DependencyProperty.Register(
-            "ShowMaximizeBox", typeof(Boolean), typeof(MesWindowOptionsBehavior),
-            new PropertyMetadata(true, OnWindowOptionChanged));
+    //    public static readonly DependencyProperty ShowMaximizeBoxProperty = DependencyProperty.Register(
+    //        "ShowMaximizeBox", typeof(Boolean), typeof(MesWindowOptionsBehavior),
+    //        new PropertyMetadata(true, OnWindowOptionChanged));
 
-        public Boolean ShowMaximizeBox
-        {
-            get => (Boolean)GetValue(ShowMaximizeBoxProperty);
-            set => SetValue(ShowMaximizeBoxProperty, value);
-        }
+    //    public Boolean ShowMaximizeBox
+    //    {
+    //        get => (Boolean)GetValue(ShowMaximizeBoxProperty);
+    //        set => SetValue(ShowMaximizeBoxProperty, value);
+    //    }
 
-        private static void OnWindowOptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((MesWindowOptionsBehavior)d).UpdateWindowStyle();
+    //    private static void OnWindowOptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((MesWindowOptionsBehavior)d).UpdateWindowStyle();
 
-        private void UpdateWindowStyle()
-        {
-            if (AssociatedObject == null)
-            {
-                return;
-            }
+    //    private void UpdateWindowStyle()
+    //    {
+    //        if (AssociatedObject == null)
+    //        {
+    //            return;
+    //        }
 
-            IntPtr handle = new WindowInteropHelper(AssociatedObject).Handle;
+    //        IntPtr handle = new WindowInteropHelper(AssociatedObject).Handle;
 
-            Int32 windowStyle = NativeMethods.GetWindowLong(handle, NativeMethods.GWL_STYLE);
+    //        Int32 windowStyle = NativeMethods.GetWindowLong(handle, NativeMethods.GWL_STYLE);
 
-            if (ShowMinimizeBox)
-            {
-                windowStyle |= NativeMethods.WS_MINIMIZEBOX;
-            }
-            else
-            {
-                windowStyle &= ~NativeMethods.WS_MINIMIZEBOX;
-            }
+    //        if (ShowMinimizeBox)
+    //        {
+    //            windowStyle |= NativeMethods.WS_MINIMIZEBOX;
+    //        }
+    //        else
+    //        {
+    //            windowStyle &= ~NativeMethods.WS_MINIMIZEBOX;
+    //        }
 
-            if (ShowMaximizeBox)
-            {
-                windowStyle |= NativeMethods.WS_MAXIMIZEBOX;
-            }
-            else
-            {
-                windowStyle &= ~NativeMethods.WS_MAXIMIZEBOX;
-            }
+    //        if (ShowMaximizeBox)
+    //        {
+    //            windowStyle |= NativeMethods.WS_MAXIMIZEBOX;
+    //        }
+    //        else
+    //        {
+    //            windowStyle &= ~NativeMethods.WS_MAXIMIZEBOX;
+    //        }
 
-            NativeMethods.SetWindowLong(handle, NativeMethods.GWL_STYLE, windowStyle);
+    //        NativeMethods.SetWindowLong(handle, NativeMethods.GWL_STYLE, windowStyle);
 
-            if (ShowIcon)
-            {
-                // TODO
-            }
-            else
-            {
-                Int32 exWindowStyle = NativeMethods.GetWindowLong(handle, NativeMethods.GWL_EXSTYLE);
-                NativeMethods.SetWindowLong(handle, NativeMethods.GWL_EXSTYLE,
-                    exWindowStyle | NativeMethods.WS_EX_DLGMODALFRAME);
+    //        if (ShowIcon)
+    //        {
+    //            // TODO
+    //        }
+    //        else
+    //        {
+    //            Int32 exWindowStyle = NativeMethods.GetWindowLong(handle, NativeMethods.GWL_EXSTYLE);
+    //            NativeMethods.SetWindowLong(handle, NativeMethods.GWL_EXSTYLE,
+    //                exWindowStyle | NativeMethods.WS_EX_DLGMODALFRAME);
 
-                NativeMethods.SetWindowPos(handle, IntPtr.Zero, 0, 0, 0, 0,
-                    NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOZORDER | NativeMethods.SWP_FRAMECHANGED);
+    //            NativeMethods.SetWindowPos(handle, IntPtr.Zero, 0, 0, 0, 0,
+    //                NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOZORDER | NativeMethods.SWP_FRAMECHANGED);
 
-                NativeMethods.SendMessage(handle, NativeMethods.WM_SETICON, IntPtr.Zero, IntPtr.Zero);
-            }
-        }
+    //            NativeMethods.SendMessage(handle, NativeMethods.WM_SETICON, IntPtr.Zero, IntPtr.Zero);
+    //        }
+    //    }
 
-        protected override void OnAttached()
-        {
-            AssociatedObject.SourceInitialized += OnSourceInitialized;
-            base.OnAttached();
-        }
+    //    protected override void OnAttached()
+    //    {
+    //        AssociatedObject.SourceInitialized += OnSourceInitialized;
+    //        base.OnAttached();
+    //    }
 
-        protected override void OnDetaching()
-        {
-            AssociatedObject.SourceInitialized -= OnSourceInitialized;
-            base.OnDetaching();
-        }
+    //    protected override void OnDetaching()
+    //    {
+    //        AssociatedObject.SourceInitialized -= OnSourceInitialized;
+    //        base.OnDetaching();
+    //    }
 
-        private void OnSourceInitialized(Object sender, EventArgs e) => UpdateWindowStyle();
-    }
+    //    private void OnSourceInitialized(Object sender, EventArgs e) => UpdateWindowStyle();
+    //}
 }
