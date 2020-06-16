4e1a7e5e0f472640b86db5138d4e2f85f7db663e
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2020-06-15 20:01:42 -0400

    experimental theme editor

---
+++ Demos/MinoriDemo/MinoriDemo.Core/ViewModels/ThemeEditorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MinoriEditorShell.Services;
+
+namespace MinoriDemo.Core.ViewModels
+{
+    class ThemeEditorViewModel : MesDocument
+    {
+        public ThemeEditorViewModel()
+        {
+            DisplayName = "Theme Editor";
+        }
+    }
+}
