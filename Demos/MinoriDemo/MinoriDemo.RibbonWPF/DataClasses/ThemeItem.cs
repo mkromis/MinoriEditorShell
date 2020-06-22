d28e52fc984ec445cc14c1c21ac3909b1ce965f2
Mark Kromis <mkromis@outlook.com>
2020-06-22 00:03:41 -0400

    fix rename

--- Demos/MinoriDemo/MinoriDemo.RibbonWPF/DataClasses/ThemeItem.cs
+++ Demos/MinoriDemo/MinoriDemo.RibbonWPF/DataClasses/ThemeItem.cs
@@ -1,11 +1,9 @@
-﻿using MvvmCross;
+﻿using MinoriDemo.RibbonWPF.Modules.Themes;
+using MvvmCross;
 using MvvmCross.Base;
-using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Net.PeerToPeer.Collaboration;
-using System.Windows;
 using System.Windows.Media;
 
 namespace MinoriDemo.RibbonWPF.DataClasses
@@ -13,25 +11,29 @@ namespace MinoriDemo.RibbonWPF.DataClasses
     class ThemeItem : MvxNotifyPropertyChanged
     {
         private Color _color;
-        private String oldKey;
-        private String key;
-        private Boolean canEdit = false;
-        private String modeText = "Edit";
+        private String _key;
+        private Boolean _canEdit = false;
+        private String _modeText = "Edit";
 
         public String ModeText { 
-            get => modeText; 
-            set => SetProperty(ref modeText, value); 
+            get => _modeText; 
+            set => SetProperty(ref _modeText, value); 
         }
         public Boolean CanEdit
         {
-            get => canEdit;
-            set => SetProperty(ref canEdit, value);
+            get => _canEdit;
+            set
+            {
+                if(SetProperty(ref _canEdit, value))
+                {
+                    ModeText = value ? "Done" : "Edit";
+                }
+            }
         }
-        public ResourceDictionary Resource { get; internal set; }
         public String Key
         {
-            get => key;
-            set => SetProperty(ref key, value);
+            get => _key;
+            set => SetProperty(ref _key, value);
         }
         public Color Color
         {
@@ -40,7 +42,7 @@ namespace MinoriDemo.RibbonWPF.DataClasses
             {
                 _color = value;
 
-                if (Resource == null)
+                if (ThemeHelper == null)
                 {
                     return;
                 }
@@ -48,97 +50,14 @@ namespace MinoriDemo.RibbonWPF.DataClasses
                 Mvx.IoCProvider.Resolve<IMvxMainThreadAsyncDispatcher>()
                 .ExecuteOnMainThreadAsync(() =>
                 {
-                    Dictionary<String, SolidColorBrush> brushes = ConvertToDictionary();
-
-                    SolidColorBrush brush = new SolidColorBrush(value);
-                    brushes[Key] = brush;
-
-                    ConvertToResource(brushes);
+                    IDictionary<String, SolidColorBrush> brushes = ThemeHelper.GetBrushes();
+                    brushes[Key] = new SolidColorBrush(value);
+                    ThemeHelper.SetBrushes(brushes);
                 });
             }
         }
 
-        public IMvxCommand EditCommand => new MvxCommand(() => RenameValues());
-
-        public void Create()
-        {
-            Dictionary<String, SolidColorBrush> brushes = ConvertToDictionary();
-
-            SolidColorBrush brush = new SolidColorBrush(Color);
-            brushes[Key] = brush;
-
-            ConvertToResource(brushes);
-        }
-
-        private void RenameValues()
-        {
-            if (!CanEdit)
-            {
-                oldKey = Key;
-                CanEdit = true;
-                ModeText = "Done";
-            }
-            else
-            {
-                CanEdit = false;
-                ModeText = "Edit";
-
-                // Set new name here
-                String newKey = Key;
-                // where !old key 
-                if (!String.IsNullOrEmpty(newKey) && newKey != oldKey)
-                {
-                    // Get list
-                    Dictionary<String, SolidColorBrush> brushes = ConvertToDictionary();
-                    
-                    // add new key
-                    brushes[newKey] = brushes[oldKey];
-                    brushes.Remove(oldKey);
-
-                    // update
-                    ConvertToResource(brushes);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Convert dictionary to resources
-        /// </summary>
-        /// <param name="brushes"></param>
-        private void ConvertToResource(Dictionary<String, SolidColorBrush> brushes)
-        {
-            // Reset visuals
-            // Setup app style
-            ResourceDictionary appTheme = Application.Current.Resources.MergedDictionaries[0];
-            appTheme.BeginInit();
-
-            appTheme.Clear();
-
-            // Object type not known at this point
-            foreach (String key2 in brushes.Keys)
-            {
-                Resource[key2] = brushes[key2];
-            }
-
-            appTheme.EndInit();
-        }
-
-        /// <summary>
-        /// Convert brushes into dictionary. This will convert old key to newKey
-        /// </summary>
-        /// <returns></returns>
-        private Dictionary<String, SolidColorBrush> ConvertToDictionary()
-        {
-            Dictionary<String, SolidColorBrush> brushes = new Dictionary<String, SolidColorBrush>();
-            foreach (String item in Resource.Keys)
-            {
-                Object current = Resource[item];
-                if (current is SolidColorBrush brush)
-                {
-                    brushes[item] = brush;
-                }
-            }
-            return brushes;
-        }
+        public ThemeHelper ThemeHelper { get; internal set; }
+        public String OriginalKey { get; internal set; }
     }
 }
---
+++ Demos/MinoriDemo/MinoriDemo.RibbonWPF/Modules/Themes/ThemeHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MinoriDemo.RibbonWPF.Modules.Themes
+{
+    class ThemeHelper
+    {
+        public ResourceDictionary Dictionary { get; set; }
+
+        /// <summary>
+        /// Gets all of the brushes in a dictionary format
+        /// </summary>
+        /// <returns></returns>
+        public IDictionary<String, SolidColorBrush> GetBrushes()
+        {
+            SortedDictionary<String, SolidColorBrush> brushes = new SortedDictionary<String, SolidColorBrush>();
+            foreach (String item in Dictionary.Keys)
+            {
+                Object current = Dictionary[item];
+                if (current is SolidColorBrush brush)
+                {
+                    brushes[item] = brush;
+                }
+            }
+            return brushes;
+        }
+
+        /// <summary>
+        /// Convert dictionary to resources
+        /// </summary>
+        /// <param name="brushes"></param>
+        public void SetBrushes(IDictionary<String, SolidColorBrush> brushes)
+        {
+            // Reset visuals
+            // Setup app style
+            Dictionary.BeginInit();
+            Dictionary.Clear();
+
+            // Object type not known at this point
+            foreach (String key in brushes.Keys)
+            {
+                Dictionary[key] = brushes[key];
+            }
+
+            Dictionary.EndInit();
+        }
+
+        public String ExportString(Boolean core, Boolean ribbon)
+        {
+            StringBuilder export = new StringBuilder();
+            export.AppendLine("<ResourceDictionary");
+            export.AppendLine("    xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"");
+            export.AppendLine("    xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\"");
+            export.AppendLine("    xmlns:System=\"clr-namespace:System;assembly=System.Runtime\"");
+            export.AppendLine("    xmlns:mc=\"http://schemas.openxmlformats.org/markup-compatibility/2006\"");
+            export.AppendLine("    xmlns:options=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation/options\"");
+            export.AppendLine("    mc:Ignorable=\"options\">");
+            export.AppendLine("    <ResourceDictionary.MergedDictionaries>");
+            if (core)
+            {
+                export.AppendLine("        <ResourceDictionary Source=\"pack://application:,,,/MahApps.Metro;component/Styles/Controls.xaml\" />");
+                export.AppendLine("        <ResourceDictionary Source=\"pack://application:,,,/MahApps.Metro;component/Styles/Fonts.xaml\" />");
+                export.AppendLine("        <ResourceDictionary Source=\"pack://application:,,,/MahApps.Metro;component/Styles/VS/Controls.xaml\" />");
+                export.AppendLine("        <ResourceDictionary Source=\"pack://application:,,,/AvalonDock.Themes.VS2013;component/Themes/Generic.xaml\" />");
+            }
+            if (ribbon)
+            {
+                export.AppendLine("        <ResourceDictionary Source=\"pack://application:,,,/Fluent;Component/Themes/Generic.xaml\" />");
+            }
+            export.AppendLine("    </ResourceDictionary.MergedDictionaries>");
+            export.AppendLine("");
+            export.AppendLine("    <!-- Begin SolidColorBrush Export -->");
+
+            IEnumerable<String> keys = GetBrushes().Keys.OrderBy(x => x);
+            if (core && !ribbon)
+            {
+                keys = keys.Where(x => !x.StartsWith("Fluent"));
+            }
+            if (!core && ribbon)
+            {
+                keys = keys.Where(x => x.StartsWith("Fluent"));
+            }
+
+            foreach (String key in keys)
+            {
+                switch (Dictionary[key])
+                {
+                    case SolidColorBrush solidColor:
+                        export.AppendLine($"    <SolidColorBrush x:Key=\"{key}\" Color=\"{solidColor.Color}\"  options:Freeze=\"True\" />");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            export.AppendLine("    <!-- End SolidColorBrush Export -->");
+            export.AppendLine("</ResourceDictionary>");
+            return export.ToString();
+        }
+
+    }
+}
