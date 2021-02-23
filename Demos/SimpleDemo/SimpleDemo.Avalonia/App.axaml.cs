9569f7f000e1acbc9adc319fad12c60937933317
Mark Kromis <mkromis@outlook.com>
2021-02-22 21:00:46 -0500

    add app setup

--- Demos/SimpleDemo/SimpleDemo.Avalonia/App.axaml.cs
+++ Demos/SimpleDemo/SimpleDemo.Avalonia/App.axaml.cs
@@ -1,15 +1,19 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using MinoriEditorShell.Platforms.Avalonia;
+using MinoriEditorShell.Platforms.Avalonia.Views;
+using MvvmCross.Core;
 
 namespace SimpleDemo.Avalonia
 {
-    public class App : Application
+    public class App : MesApplication
     {
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
         }
+        protected override void RegisterSetup() => this.RegisterSetupType<MesAvnSetup<Core.App>>();
 
         public override void OnFrameworkInitializationCompleted()
         {
