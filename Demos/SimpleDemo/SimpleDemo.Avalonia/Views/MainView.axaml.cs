97e7cd304c6e3fa541edba7d6326ea20e971408e
Mark Kromis <mkromis@outlook.com>
2021-02-22 21:47:08 -0500

    sample setup of mainview

---
+++ Demos/SimpleDemo/SimpleDemo.Avalonia/Views/MainView.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using MinoriEditorShell.Platforms.Avalonia.Views;
+
+namespace SimpleDemo.Avalonia.Views
+{
+    public class MainView : MesAvnView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
