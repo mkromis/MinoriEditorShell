fe7a2bae788926ccfdb419436ba6645e414b02c6
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2021-04-10 17:50:13 -0400

    Playing with blazor web app

---
+++ Demos/SimpleDemo/SimpleDemo.BlazorWebAssembly/Shared/NavMenu.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleDemo.BlazorWebAssembly.Shared
+{
+    public partial class NavMenu
+    {
+        private Boolean collapseNavMenu = true;
+
+        private String NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
