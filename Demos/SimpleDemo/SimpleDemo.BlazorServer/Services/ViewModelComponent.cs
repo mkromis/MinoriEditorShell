74c61deaf8de56792fbdc7d52d7b9fe1eb815a5e
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2021-05-09 17:42:32 -0400

    Tipview sample implementation

--- Demos/SimpleDemo/SimpleDemo.BlazorServer/Services/ViewModelComponent.cs
+++ Demos/SimpleDemo/SimpleDemo.BlazorServer/Services/ViewModelComponent.cs
@@ -1,25 +1,24 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MvvmCross.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace SimpleDemo.BlazorServer.Services
 {
+    /// <summary>
+    /// Helper for MvxViewModel support
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="https://bartecki.me/blog/mvvm-in-blazor"/>
     public abstract class ViewModelComponent<T> : ComponentBase where T : MvxViewModel
     {
         [Inject] 
         public T ViewModel { get; set; }
 
-        protected override Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync()
         {
-            ViewModel.PropertyChanged += (o, e) => OnViewModelUpdate();
-            return base.OnInitializedAsync();
-        }
-        protected virtual void OnViewModelUpdate()
-        {
-            StateHasChanged();
+            ViewModel.PropertyChanged += (o, e) => StateHasChanged();
+            await ViewModel.Initialize(); 
+            await base.OnInitializedAsync();
         }
     }
 }
