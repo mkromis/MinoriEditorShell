6dd381f6ad700753bc3b32ed5fd27864522f1a29
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2021-05-08 20:53:57 -0400

    use code behind

---
+++ Demos/SimpleDemo/SimpleDemo.BlazorServer/Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleDemo.BlazorServer.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
---
+++ Demos/SimpleDemo/SimpleDemo.BlazorServer/Pages/FetchData.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleDemo.BlazorServer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleDemo.BlazorServer.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+
+        [Inject] 
+        WeatherForecastService ForecastService { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
