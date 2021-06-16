c9b2fb985a50a78b9afdb1989d1585b7812d4903
Mark Kromis <mkromis@outlook.com>
2021-06-15 22:50:33 -0400

    net5 tweaks

---
+++ Tests/MinoriEditorShellTests/Converters/CultureInfoNameConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinoriEditorShell.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace MinoriEditorShell.ConvertersTests
+{
+    [TestClass]
+    public class CultureInfoNameConverterTests
+    {
+        [TestMethod]
+        public void SystemTest()
+        {
+            CultureInfoNameConverter converter = new();
+        }
+
+        [TestMethod]
+        public void ConvertBackFailTest()
+        {
+            CultureInfoNameConverter converter = new();
+            Assert.ThrowsException<NotSupportedException>(() => converter.ConvertBack(new Object(), typeof(Object), new Object(), CultureInfo.CurrentCulture));
+        }
+    }
+}
