2891656e5b7af9308e12f6ed015c41125cfd59dd
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2021-07-31 22:36:33 -0400

    Fix logging

---
+++ Demos/SimpleDemo/SimpleDemo.Avalonia/Setup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+using MinoriEditorShell.Platforms.Avalonia;
+
+namespace SimpleDemo.Avalonia
+{
+    internal class Setup : MesAvnSetup<Core.App>
+    {
+        protected override ILoggerFactory CreateLogFactory() => LoggerFactory.Create(b => b.SetMinimumLevel(0).AddSimpleConsole(o => o.SingleLine = true));
+        protected override ILoggerProvider CreateLogProvider() => null;
+    }
+}
