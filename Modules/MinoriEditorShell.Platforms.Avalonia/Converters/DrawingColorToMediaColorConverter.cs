760b99e7964184a0f1c6f1d2245d34debffb2e7c
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2021-08-22 22:09:24 -0400

    clean up some converters

--- Modules/MinoriEditorShell.Platforms.Avalonia/Converters/DrawingColorToMediaColorConverter.cs
+++ Modules/MinoriEditorShell.Platforms.Avalonia/Converters/DrawingColorToMediaColorConverter.cs
@@ -1,24 +1,32 @@
-﻿using System;
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
 using System.Globalization;
-// using System.Windows.Data;
-// using System.Windows.Media;
 
 namespace MinoriEditorShell.Platforms.Avalonia.Converters
 {
-    // public class DrawingColorToMediaColorConverter : IValueConverter
-    // {
-    //     // Convert from Drawing Color to meda brush
-    //     public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
-    //     {
-    //         System.Drawing.Color? color = value as System.Drawing.Color?;
-    //         return color == null ? null : (Object)System.Windows.Media.Color.FromArgb(color.Value.A, color.Value.R, color.Value.G, color.Value.B);
-    //     }
+    /// <summary>
+    /// Drawing color to media color
+    /// </summary>
+    public class DrawingColorToMediaColorConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert from Drawing Color to meda brush
+        /// </summary>
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            System.Drawing.Color? color = value as System.Drawing.Color?;
+            return color == null ? null : (Object)Color.FromArgb(color.Value.A, color.Value.R, color.Value.G, color.Value.B);
+        }
 
-    //     public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
-    //     {
-    //         Color? color = value as Color?;
-    //         return color == null ? System.Drawing.Color.Empty :
-    //             System.Drawing.Color.FromArgb(color.Value.A, color.Value.R, color.Value.G, color.Value.B);
-    //     }
-    // }
+        /// <summary>
+        /// Back convert to drawing color from media brush
+        /// </summary>
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            Color? color = value as Color?;
+            return color == null ? System.Drawing.Color.Empty :
+                System.Drawing.Color.FromArgb(color.Value.A, color.Value.R, color.Value.G, color.Value.B);
+        }
+    }
 }
