302a67c2e8df68f958e2ba52f2edddcb747df4cc
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2021-08-23 22:15:37 -0400

    remove un-used classes

--- Modules/MinoriEditorShell/Threading/MesTaskUtility.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace MinoriEditorShell.Threading
-{
-    public class MesTaskUtility
-    {
-        public static readonly Task Completed = Task.FromResult(true);
-    }
-}
