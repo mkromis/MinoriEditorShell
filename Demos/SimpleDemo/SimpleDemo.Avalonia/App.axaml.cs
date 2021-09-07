38771350da19dff0323fa41874928587b21dc0cf
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2021-09-06 21:31:36 -0400

    remove messages

--- Demos/SimpleDemo/SimpleDemo.Avalonia/App.axaml.cs
+++ Demos/SimpleDemo/SimpleDemo.Avalonia/App.axaml.cs
@@ -11,11 +11,11 @@ namespace SimpleDemo.Avalonia
     public class App : MesApplication
     {
         public override void Initialize() => AvaloniaXamlLoader.Load(this);
+
         protected override void RegisterSetup() => this.RegisterSetupType<Setup>();
 
         public override void OnFrameworkInitializationCompleted()
         {
-            Debug.WriteLine("InitTest");
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow();
